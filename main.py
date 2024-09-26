9329d8258f1f7f3deebab1cfd6123661b9f55a85
Malte <malte.roesgen@gmail.com>
2024-09-26 20:33:37 +0200

    add max size & only create level.json if it does not exist.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 from LevelGenerator import level
 from fileUtil import createFile, writeLevel
@@ -14,18 +15,33 @@ except IndexError:
     devMode = False
 
 def main():
-    createFile()
-    
+    if not os.path.exists("level.json"):
+        createFile()
+
     if devMode:
         print("Developer mode enabled")
     else:
         print("Developer mode disabled")
 
         width = int(input("Enter the width of the level: "))
+
+        if width > config.getSettings()["maxSize"]:
+            print(f"Width cannot be greater than {config.getSettings()['maxSize']}")
+            input("Press enter to exit...")
+            sys.exit()
+
         height = int(input("Enter the height of the level: "))
         
+
+        if height > config.getSettings()["maxSize"]:
+            print(f"Height cannot be greater than {config.getSettings()['maxSize']}")
+            input("Press enter to exit...")
+            sys.exit()
+
         level.generate(width, height)
-    
+
+        writeLevel(level.level)
+
         print("Level generated and saved to level.json")
         print("You can edit the level.json file to change the level")
         input("Press enter to continue...")
@@ -33,6 +49,7 @@ def main():
     
 
     print("Level generation complete")
+
     print("Starting rendering...")
     
     print(config.get())
